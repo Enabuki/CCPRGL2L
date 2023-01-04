82d97d2618c21ebfe26d8d7fce0b2accb14f8cb7
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2023-01-05 01:32:18 +0800

    Create lab1act3.java

---
+++ Laboratory1/lab1act3.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class lab1act3 {
+
+ 
+    public static void main(String[] args) {
+     int vowelCount = 0;
+
+
+    try (BufferedReader reader = new BufferedReader(new FileReader("dummy.txt"))) {
+      String line;
+      int lineCount = 0;
+      while ((line = reader.readLine()) != null && lineCount < 5) {
+        for (char c : line.toCharArray()) {
+          if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            vowelCount++;
+          }
+        }
+        System.out.println(line);     
+        lineCount++;
+      }
+    } catch (IOException e) {
+        System.out.println("There's an error in the code");
+    }
+
+    System.out.println("Total number of vowels: " + vowelCount);
+  }
+}
