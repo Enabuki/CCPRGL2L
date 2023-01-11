eeca86adebda7a1f9d25051ff0044e993214268f
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2023-01-12 05:52:58 +0800

    Update and rename lab1num5.java to lab1no5.java

--- Laboratory1/lab1no5.java
+++ Laboratory1/lab1no5.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class lab1num5 {
+public class lab1no5 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
